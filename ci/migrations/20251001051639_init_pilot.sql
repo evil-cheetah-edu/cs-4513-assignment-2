30840e5529e7c0bd2def07f2d250f5b1306e07b4
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2025-10-01 00:19:16 -0500

    add(migration): pilot table

---
+++ ci/migrations/20251001051639_init_pilot.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE Pilot (
+    plid    INT            NOT NULL,
+    plname  VARCHAR(255)   NOT NULL,
+    hours   REAL           NOT NULL,
+    
+    PRIMARY KEY (plid)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE Pilot
+-- +goose StatementEnd
