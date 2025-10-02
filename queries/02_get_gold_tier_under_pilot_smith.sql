e7982301029890bd80ba45c5c7462e2bdcebab7c
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2025-10-02 14:12:26 -0500

    refactor(query): used lower function against strings and gave more meaningful names

--- queries/02_get_gold_tier_under_pilot_smith.sql
+++ queries/02_get_gold_tier_under_pilot_smith.sql
@@ -1,9 +1,13 @@
 -- Author: Donovan Semenuk
 -- Username: @DonovanSemenuk
-SELECT DISTINCT p.pname
-FROM Passenger AS p
-JOIN Booking  AS b  ON b.pid  = p.pid
-JOIN Flight   AS f  ON f.fnum = b.fnum
-JOIN Pilot    AS pl ON pl.plid = f.plid
-WHERE p.tier = 'Gold'
-    AND pl.plname = 'Smith';
+SELECT
+    DISTINCT passenger.pname
+FROM Passenger AS passenger
+JOIN Booking AS booking
+    ON booking.pid  = passenger.pid
+JOIN Flight AS flight
+    ON flight.fnum = booking.fnum
+JOIN Pilot AS pilot
+    ON pilot.plid = flight.plid
+WHERE LOWER(passenger.tier) = 'gold'
+    AND LOWER(pilot.plname) = 'smith';
