d5d6d7aa9a1fec28973feef5333505d7940b7e21
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2025-10-02 14:24:55 -0500

    refactor(query): by using lower against strings

--- queries/03_get_passengers_oldest_age_having_silver_tier_or_piloted_by_smith.sql
+++ queries/03_get_passengers_oldest_age_having_silver_tier_or_piloted_by_smith.sql
@@ -1,13 +1,20 @@
 -- Author: Donovan Semenuk
 -- Username: @DonovanSemenuk
-SELECT MAX(p.age) AS oldest_age
-FROM Passenger AS p
-WHERE p.tier = 'Silver'
-   OR EXISTS (
-        SELECT 1
-        FROM Booking b
-        JOIN Flight  f  ON f.fnum = b.fnum
-        JOIN Pilot   pl ON pl.plid = f.plid
-        WHERE b.pid = p.pid
-          AND pl.plname = 'Smith'
-   );
+SELECT MAX(passenger.age) AS oldest_age
+FROM Passenger AS passenger
+WHERE LOWER(passenger.tier) = 'silver'
+OR
+EXISTS (
+    SELECT 1
+    FROM Booking AS booking
+
+    JOIN Flight AS flight
+        ON flight.fnum = booking.fnum
+    JOIN Pilot AS pilot
+        ON pilot.plid = flight.plid
+
+    WHERE
+        booking.pid = passenger.pid
+        AND
+        LOWER(pilot.plname) = 'smith'
+);
